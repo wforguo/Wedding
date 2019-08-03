9352fc1f025a75a2ffe0a59cc02a20fa024c55c2
forguo <forguo@dazwang.com>
2019-08-03 22:42:01 +0800

    add user login

--- wedding-server/manage/routes/index.js
+++ wedding-server/manage/routes/index.js
@@ -1,22 +1,24 @@
 const router = require('koa-router')();
 const config = require('../../config');
 const mongoose = require('mongoose');
-const Goods = require('../../models/goods');
+const Album = require('../../models/album');
+const User = require('../../models/user');
+require('./../../util/util');
 
 //连接MongoDB数据库
 mongoose.connect(`${config.database.url}:${config.database.port}/${config.database.name}`, { useNewUrlParser: true });
 // mongoose.connect('mongodb+srv://admin:root@shop-p0l56.gcp.mongodb.net/dumall?retryWrites=true&w=majority', { useNewUrlParser: true });
-
+// mongoose.connect('mongodb+srv://root:admin@cluster0-acqfy.azure.mongodb.net/wedding?retryWrites=true&w=majority', { useNewUrlParser: true });
 mongoose.connection.on("connected", function () {
-    console.log("MongoDB connected success.")
+  console.log("MongoDB connected success.")
 });
 
 mongoose.connection.on("error", function () {
-    console.log("MongoDB connected fail.")
+  console.log("MongoDB connected fail.")
 });
 
 mongoose.connection.on("disconnected", function () {
-    console.log("MongoDB connected disconnected.")
+  console.log("MongoDB connected disconnected.")
 });
 
 /**
@@ -29,63 +31,258 @@ router.get('/', async (ctx, next) => {
 })
 
 /**
- * post商品列表
+ * 用户登录
  */
-router.post('/getList', async (ctx, next) => {
-    console.log(JSON.stringify(ctx.request.body))
-    let request = ctx.request.body;
-    let page = request.page;
-    let pageSize = 10;
-    let skip = (page - 1) * request.pageSize;
-    let params = {};
-    let res = await Goods.find(params).skip(skip).limit(pageSize);
+router.post('/manage/user/login', async (ctx, next) => {
+  if (!ctx.request.body || ctx.request.body.length === 0) {
     ctx.body = {
-        errcode: 0,
-        msg: '登录成功',
-        result:{
-            count: res.length,
-            list: res
-        }
+      errcode: 10009,
+      msg: '用户名或密码不能为空不能为空',
     };
-    // let res = goodsModel.exec(function (err,doc) {
-    //     console.log(err);
-    //     if(err){
-    //         ctx.body = {
-    //             errcode: 10000,
-    //             msg: err.errmsg
-    //         }
-    //     } else{
-    //         ctx.body = {
-    //             errcode: 0,
-    //             msg: '登录成功',
-    //             result:{
-    //                 count:doc.length,
-    //                 list:doc
-    //             }
-    //         };
-    //     }
-    // });
+    return false;
+  }
+  let request = ctx.request.body;
+  let userName = request.userName;
+  let userPwd = request.userPwd;
+  if (!userName || userName.length === 0) {
+    ctx.body = {
+      errcode: 10009,
+      msg: '用户名不能为空',
+    };
+    return false;
+  }
+  if (!userPwd || userPwd.length === 0) {
+    ctx.body = {
+      errcode: 10009,
+      msg: '密码不能为空',
+    };
+    return false;
+  }
+
+  let param = {
+    userName: userName,
+    userPwd: userPwd
+  };
+
+  let res = await User.findOne(param).catch(error => {
+    ctx.body = {
+      errcode: 10086,
+      msg:error.message
+    };
+  });
+
+  if (!res || res.length === 0) {
+    ctx.body = {
+      errcode: 10086,
+      msg: '用户名或密码错误',
+    };
+  } else {
+    ctx.body = {
+      errcode: 0,
+      msg: 'ok',
+      result: res
+    };
+  }
+})
+
+/**
+ * post照片列表
+ */
+router.post('/manage/album/list', async (ctx, next) => {
+  let request = ctx.request.body;
+  let page = request.page || 0;
+  let pageSize = request.pageSize || 10;
+  let skip = (page - 1) * pageSize;
+  let params = {};
+  let res = await Album.find(params).skip(Number(skip)).limit(Number(pageSize)).catch(error => {
+    ctx.body = {
+      errcode: 10086,
+      msg:error.message
+    };
+  });
+  ctx.body = {
+    errcode: 0,
+    msg: 'ok',
+    result:{
+      count: res.length,
+      list: res
+    }
+  };
 })
 
 
 /**
- * get商品列表
+ * get照片列表
  */
-router.get('/getList', async (ctx, next) => {
-    console.log(JSON.stringify(ctx.request))
-    let request = ctx.request.body;
-    let page = request.page;
-    let pageSize = 10;
-    let skip = (page - 1) * request.pageSize;
-    let params = {};
-    let res = await Goods.find(params).skip(skip).limit(pageSize);
+router.get('/manage/album/list', async (ctx, next) => {
+  let query = ctx.request.query; // if nothing to pass just return a {}
+  let page = query.page || 0;
+  let pageSize = query.page.pageSize || 10;
+  let skip = (page - 1) * 10;
+  let params = {};
+  let res = await Album.find(params).skip(Number(skip)).limit(Number(pageSize)).catch(error => {
     ctx.body = {
-        errcode: 0,
-        msg: '登录成功',
-        result:{
-            count: res.length,
-            list: res
-        }
+      errcode: 10086,
+      msg:error.message
     };
+  });
+  ctx.body = {
+    errcode: 0,
+    msg: 'ok',
+    result:{
+      count: res.length,
+      list: res
+    }
+  };
 })
+
+/**
+ * add照片
+ */
+router.post('/manage/album/add', async (ctx, next) => {
+  if (!ctx.request.body || ctx.request.body.length === 0) {
+    ctx.body = {
+      errcode: 10001,
+      msg: '参数不能为空',
+    };
+    return false;
+  }
+  let url = ctx.request.body.url;
+  let desc = ctx.request.body.desc;
+  if (!url || url.length === 0) {
+    ctx.body = {
+      errcode: 10000,
+      msg: '图片不能为空',
+    };
+    return false;
+  }
+  if (!desc || desc.length === 0) {
+    ctx.body = {
+      errcode: 10000,
+      msg: '图片描述能为空',
+    };
+    return false;
+  }
+  let createTime = new Date().Format('yyyy/MM/dd hh:mm:ss');
+  let platform = 'album';
+  let r1 = Math.floor(Math.random()*10);
+  let r2 = Math.floor(Math.random()*10);
+
+  let sysDate = new Date().Format('yyyyMMddhhmmss');
+  let id = platform + r1 + sysDate + r2;
+  let album = new Album({
+    id: id,
+    url: url,
+    desc: desc,
+    time: createTime
+  });
+  let res = await album.save().catch(error => {
+    ctx.body = {
+      errcode: 10086,
+      msg:error.message
+    };
+  });
+  ctx.body = {
+    errcode: 0,
+    msg: 'ok',
+    result: res
+  };
+})
+
+/**
+ * add照片
+ */
+router.get('/manage/album/add', async (ctx, next) => {
+  if (!ctx.request.query || ctx.request.query === 0) {
+    ctx.body = {
+      errcode: 10001,
+      msg: '参数不能为空',
+    };
+    return false;
+  }
+
+  let query = ctx.request.query; // if nothing to pass just return a {}
+  let url = query.url;
+  let desc = query.desc;
+  if (!url || url.length === 0) {
+    ctx.body = {
+      errcode: 10000,
+      msg: '图片不能为空',
+    };
+    return false;
+  }
+  if (!desc || desc.length === 0) {
+    ctx.body = {
+      errcode: 10000,
+      msg: '图片描述能为空',
+    };
+    return false;
+  }
+  let createTime = new Date().Format('yyyy/MM/dd hh:mm:ss');
+  let platform = 'album';
+  let r1 = Math.floor(Math.random()*10);
+  let r2 = Math.floor(Math.random()*10);
+
+  let sysDate = new Date().Format('yyyyMMddhhmmss');
+  let id = platform + r1 + sysDate + r2;
+  let album = new Album({
+    id: id,
+    url: url,
+    desc: desc,
+    time: createTime
+  });
+  let res = await album.save().catch(error => {
+    ctx.body = {
+      errcode: 10086,
+      msg:error.message
+    };
+  });
+  ctx.body = {
+    errcode: 0,
+    msg: 'ok',
+    result: res
+  };
+})
+
+/**
+ * 删除照片
+ */
+router.post('/manage/album/del', async (ctx, next) => {
+  let id = ctx.request.body.id || '';
+  let res = await Album.deleteOne({'id': id}).catch(error => {
+    ctx.body = {
+      errcode: 10086,
+      msg: error.message
+    };
+  });
+  ctx.body = {
+    errcode: 0,
+    msg: 'ok',
+    result:{
+      res: res
+    }
+  };
+})
+
+/**
+ * 删除照片
+ */
+router.get('/manage/album/del', async (ctx, next) => {
+  let query = ctx.request.query; // if nothing to pass just return a {}
+  let id = query.id || '';
+  let res = await Album.deleteOne({'id': id}).catch(error => {
+    ctx.body = {
+      errcode: 10086,
+      msg:error.message
+    };
+  });
+  ctx.body = {
+    errcode: 0,
+    msg: 'ok',
+    result:{
+      res: res
+    }
+  };
+})
+
 module.exports = router
--- wedding-server/models/album.js
+++ wedding-server/models/album.js
@@ -2,10 +2,10 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema;
 
 let albumSchema = new Schema({
-    id: String,
-    url: String,
-    desc: String,
-    time: String
+  id: String,
+  url: String,
+  desc: String,
+  time: String
 });
 
 module.exports = mongoose.model('Album', albumSchema, 'wedd-album');
