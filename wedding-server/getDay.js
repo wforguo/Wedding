f5f92dad2379960bb1fbe656b96bf812f30320d0
魏国 <forguo@weiguodeMacBook-Pro.local>
2019-12-07 22:34:30 +0800

    修改一些东西

--- wedding-server/getDay.js
+++
@@ -1,5 +0,0 @@
-let getDay = function (start, end) {
-    let between = new Date(end) - new Date(start);
-    return between / (60 * 60 * 1000 * 24);
-};
-console.log(getDay('2019/07/08', '2020/06/04'));
